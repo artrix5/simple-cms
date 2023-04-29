9a687b27d0b58cd9ec580a5cc414f29e447ec3b5
Artrix555 <adrian01052002@gmail.com>
2023-04-30 00:27:29 +0200

    Added PHP files.

---
+++ script.php
@@ -0,0 +1,42 @@
+<?php
+
+$serverName = "localhost:3306";
+$username = "root";
+$password = "";
+$db = "Welt";
+
+$dbc = mysqli_connect($serverName, $username, $password, $db) or die("Error" . mysqli_connect_error());
+
+if (isset($_POST['title']) && isset($_POST['summary']) && isset($_POST['text']) && isset($_POST['category']) && isset($_POST['picture']) && isset($_POST['date'])) {
+
+    $title = $_POST['title'];
+    $summary = $_POST['summary'];
+    $text = $_POST['text'];
+    $category = $_POST['category'];
+    $picture = $_POST['picture'];
+    $date = $_POST['date'];
+
+    //$checkbox = isset($_POST['checkbox']) ? 'Yes' : 'No';
+
+    // Insert form data into database
+    $query = "INSERT INTO test (title, summary, knjiga, category, picture, datum) 
+          VALUES ('$title', '$summary', '$text', '$category', '$picture', '$date')";
+    $result = mysqli_query($dbc, $query) or die("Error" . mysqli_error($dbc));
+
+    if ($result) {
+        // Display success message
+        echo "Form data successfully added to the database.";
+    } else {
+        // Display error message
+        echo "Error adding form data to the database.";
+    }
+
+} else {
+    echo "Enter all fields before submitting.";
+}
+
+mysqli_close($dbc);
+
+?>
+
+
